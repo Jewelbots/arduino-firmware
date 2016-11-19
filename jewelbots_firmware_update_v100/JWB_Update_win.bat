922ff3778f437f5e9b2e2f5065289f5c5bbd7bbe
Scott Davison <scott@jewelbots.com>
2016-11-19 06:22:01 -0500

    add back in verbose - lets people know what is going on

--- jewelbots_firmware_update_v100/JWB_Update_win.bat
+++ jewelbots_firmware_update_v100/JWB_Update_win.bat
@@ -3,5 +3,5 @@ echo off
 set PATH=%1
 set PORT=%2
 
-%PATH%/JWBLoader.exe  dfu serial --package=%PATH%/JWB_update_1_01a.zip --port=%PORT% --baudrate=38400
-%PATH%/JWBLoader.exe  dfu serial --package=%PATH%/JWB_update_1_01b.zip --port=%PORT% --baudrate=38400
+%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_01a.zip --port=%PORT% --baudrate=38400
+%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_01b.zip --port=%PORT% --baudrate=38400

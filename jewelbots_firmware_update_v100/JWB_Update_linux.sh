ff9e2ad8df0fd937df00344e08b4244f8e7a342e
Scott Davison <scott@jewelbots.com>
2016-12-07 14:18:59 -0500

    Update deployed firmware with new feature.  Button press makes a red light flash to indicate the Jewelbot is still alive.

--- jewelbots_firmware_update_v100/JWB_Update_linux.sh
+++ jewelbots_firmware_update_v100/JWB_Update_linux.sh
@@ -2,5 +2,5 @@
 
 PATH="$1"
 PORT="$2"
-$PATH/JWBLoader_linux --verbose dfu serial --package=$PATH/JWB_update_1_02a.zip --port=$PORT --baudrate=38400
-$PATH/JWBLoader_linux --verbose dfu serial --package=$PATH/JWB_update_1_02b.zip --port=$PORT --baudrate=38400
+$PATH/JWBLoader_linux --verbose dfu serial --package=$PATH/JWB_update_1_03a.zip --port=$PORT --baudrate=38400
+$PATH/JWBLoader_linux --verbose dfu serial --package=$PATH/JWB_update_1_03b.zip --port=$PORT --baudrate=38400
--- jewelbots_firmware_update_v100/JWB_Update_win.bat
+++ jewelbots_firmware_update_v100/JWB_Update_win.bat
@@ -6,6 +6,6 @@ set PATH=%1
 set PORT=%2
 
 
-%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_02a.zip --port=%PORT% --baudrate=38400
+%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_03a.zip --port=%PORT% --baudrate=38400
 
-%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_02b.zip --port=%PORT% --baudrate=38400
+%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_03b.zip --port=%PORT% --baudrate=38400

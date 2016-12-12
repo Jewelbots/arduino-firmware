f3fa2d524ebeb9c8f4ba0eb03d5aa3a1420321a2
Scott Davison <scott@jewelbots.com>
2016-12-12 15:42:22 -0500

    New and improved messaging mode

--- jewelbots_firmware_update_v100/JWB_Update_win.bat
+++ jewelbots_firmware_update_v100/JWB_Update_win.bat
@@ -6,6 +6,6 @@ set PATH=%1
 set PORT=%2
 
 
-%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_03a.zip --port=%PORT% --baudrate=38400
+%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_04a.zip --port=%PORT% --baudrate=38400
 
-%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_03b.zip --port=%PORT% --baudrate=38400
+%PATH%/JWBLoader.exe --verbose dfu serial --package=%PATH%/JWB_update_1_04b.zip --port=%PORT% --baudrate=38400

a2609e6c9b2048818d42857d8da1c81c8b107f44
Scott Davison <scott@jewelbots.com>
2017-01-04 15:03:57 -0500

    correct the issue on Linux systems

--- jewelbots_firmware_update_v100/JWB_Update_linux.sh
+++ jewelbots_firmware_update_v100/JWB_Update_linux.sh
@@ -3,4 +3,5 @@
 PATH="$1"
 PORT="$2"
 $PATH/JWBLoader_linux --verbose dfu serial --package=$PATH/JWB_update_1_04a.zip --port=$PORT --baudrate=38400
+/bin/sleep 1s
 $PATH/JWBLoader_linux --verbose dfu serial --package=$PATH/JWB_update_1_04b.zip --port=$PORT --baudrate=38400
